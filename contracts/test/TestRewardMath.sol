116290e14805a63fff01d4db55f05bdc2b271dbf
Aliver <aliver.len@outlook.com>
2024-01-24 15:12:02 +0800

    chore: test util

--- contracts/test/TestRewardMath.sol
+++ contracts/test/TestRewardMath.sol
@@ -31,4 +31,16 @@ contract TestRewardMath {
             currentTime
         );
     }
+
+    function computeRewardDistribution(
+        uint256 reward,
+        uint32 stakedSince,
+        uint32 penaltyDecreasePeriod
+    ) internal view returns (uint256 ownerEarning, uint256 liquidatorEarning, uint256 refunded) {
+        (ownerEarning, liquidatorEarning, refunded) = RewardMath.computeRewardDistribution(
+            reward,
+            stakedSince,
+            penaltyDecreasePeriod
+        );
+    }
 }
